e1350a9b40cfd09966ac8114ca3cb99a3966a13c
Blade242141 <blade242141yolo@gmail.com>
2023-01-24 14:43:55 +0000

    Init Commit

---
+++ CSC8503/TutorialGame.h
@@ -0,0 +1,91 @@
+#pragma once
+#include "GameTechRenderer.h"
+#ifdef USEVULKAN
+#include "GameTechVulkanRenderer.h"
+#endif
+#include "PhysicsSystem.h"
+
+#include "StateGameObject.h"
+
+namespace NCL {
+	namespace CSC8503 {
+		class TutorialGame		{
+		public:
+			TutorialGame();
+			~TutorialGame();
+
+			virtual void UpdateGame(float dt);
+
+		protected:
+			void InitialiseAssets();
+
+			void InitCamera();
+			void UpdateKeys();
+
+			void InitWorld();
+
+			/*
+			These are some of the world/object creation functions I created when testing the functionality
+			in the module. Feel free to mess around with them to see different objects being created in different
+			test scenarios (constraints, collision types, and so on). 
+			*/
+			void InitGameExamples();
+
+			void InitSphereGridWorld(int numRows, int numCols, float rowSpacing, float colSpacing, float radius);
+			void InitMixedGridWorld(int numRows, int numCols, float rowSpacing, float colSpacing);
+			void InitCubeGridWorld(int numRows, int numCols, float rowSpacing, float colSpacing, const Vector3& cubeDims);
+
+			void InitDefaultFloor();
+
+			bool SelectObject();
+			void MoveSelectedObject();
+			void DebugObjectMovement();
+			void LockedObjectMovement();
+
+			GameObject* AddFloorToWorld(const Vector3& position);
+			GameObject* AddSphereToWorld(const Vector3& position, float radius, float inverseMass = 10.0f);
+			GameObject* AddCubeToWorld(const Vector3& position, Vector3 dimensions, float inverseMass = 10.0f);
+
+			GameObject* AddPlayerToWorld(const Vector3& position);
+			GameObject* AddEnemyToWorld(const Vector3& position);
+			GameObject* AddBonusToWorld(const Vector3& position);
+
+#ifdef USEVULKAN
+			GameTechVulkanRenderer*	renderer;
+#else
+			GameTechRenderer* renderer;
+#endif
+			PhysicsSystem*		physics;
+			GameWorld*			world;
+
+			bool useGravity;
+			bool inSelectionMode;
+
+			float		forceMagnitude;
+
+			GameObject* selectionObject = nullptr;
+
+			MeshGeometry*	capsuleMesh = nullptr;
+			MeshGeometry*	cubeMesh	= nullptr;
+			MeshGeometry*	sphereMesh	= nullptr;
+
+			TextureBase*	basicTex	= nullptr;
+			ShaderBase*		basicShader = nullptr;
+
+			//Coursework Meshes
+			MeshGeometry*	charMesh	= nullptr;
+			MeshGeometry*	enemyMesh	= nullptr;
+			MeshGeometry*	bonusMesh	= nullptr;
+
+			//Coursework Additional functionality	
+			GameObject* lockedObject	= nullptr;
+			Vector3 lockedOffset		= Vector3(0, 14, 20);
+			void LockCameraToObject(GameObject* o) {
+				lockedObject = o;
+			}
+
+			GameObject* objClosest = nullptr;
+		};
+	}
+}
+
