2e243bc5e9d16692eaa3b062a42842a20ecd0cd0
Ryan Holt <c2055347@newcastle.ac.uk>
2023-01-30 10:25:25 +0000

    Camera a bit snappier to the target

--- NCLCoreClasses/Camera.cpp
+++ NCLCoreClasses/Camera.cpp
@@ -60,7 +60,7 @@ void Camera::UpdateCamera(float dt, Vector3 targetPosition, Vector3 targetSize) 
 	Vector3 rotation = Matrix3::FromEuler(Vector3(0, yaw, pitch)) * Vector3(0, 0, targetSize.y * 5);
 	Vector3 positionDifference = targetPosition - lastTargetPosition;
 
-	positionDifference *= (dt + 0.01f);
+	positionDifference *= (dt + 0.02f);
 
 	position = lastTargetPosition + positionDifference + rotation + Vector3(0, targetSize.y * 0.25f, 0);
 
