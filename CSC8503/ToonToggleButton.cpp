2949073439717d03018db649924b634b477b6e8e
Sumit Makkar <c2060829@newcastle.ac.uk>
2023-02-28 13:50:26 +0000

    Toggle Animation working

--- CSC8503/ToonToggleButton.cpp
+++ CSC8503/ToonToggleButton.cpp
@@ -4,18 +4,19 @@ void ToonToggleButton::DrawButton()
 {
 	DrawOffToggle();
 	DrawOnToggle();
-	DrawHead();
+	m_ToggleButtonHead->UpdateHead();
 	if (m_ShouldDrawValue) { DrawValue(); }
 }
 
 
 ToonToggleButton::ToonToggleButton(Coordinates coordinates, bool shouldDrawValue)
 {
-	m_CurrentState	  = ToggleButtonStates::ToggleOff;
-	m_Coordinates	  = coordinates;
-	m_HalfSize		  = m_Coordinates;
-	m_HalfSize.size	  = m_Coordinates.size / 2;
-	m_ShouldDrawValue = shouldDrawValue;
+	m_CurrentState	   = ToggleButtonStates::ToggleOff;
+	m_Coordinates	   = coordinates;
+	m_HalfSize		   = m_Coordinates;
+	m_HalfSize.size	   = m_Coordinates.size / 2;
+	m_ShouldDrawValue  = shouldDrawValue;
+	m_ToggleButtonHead = new ToonToggleButtonHead(m_HalfSize);
 }
 
 ToonToggleButton::~ToonToggleButton()
@@ -36,7 +37,11 @@ void ToonToggleButton::UpdateButtonDraw()
 
 void ToonToggleButton::HandleEvents()
 {
-	if (InputManager::GetInstance().GetInputs()[1]->IsShooting()) { ToggleState(); }
+	if (InputManager::GetInstance().GetInputs()[1]->IsShooting())
+	{
+		ToggleCurrentState();
+		m_ToggleButtonHead->TriggerAnimation();
+	}
 }
 
 void ToonToggleButton::DrawOffToggle()
@@ -51,20 +56,6 @@ void ToonToggleButton::DrawOnToggle()
 	Debug::DrawFilledQuad(origin, m_HalfSize.size, m_ToggleOffColour);
 }
 
-void ToonToggleButton::DrawHead()
-{
-	Coordinates headCoordinates = m_CurrentState == ToggleButtonStates::ToggleOff ? m_HalfSize : Coordinates(Vector2(m_Coordinates.origin.x + m_HalfSize.size.x, m_Coordinates.origin.y), m_HalfSize.size);
-	headCoordinates.ScaleCoordinatesBy(0.15f);
-	Debug::DrawFilledQuad(headCoordinates.origin, headCoordinates.size, m_HeadColour);
-	/*headCoordinates.ScaleCoordinatesBy(0.15f);
-	Debug::DrawQuad(headCoordinates.origin, headCoordinates.size, Vector4(1.0f, 1.0f, 1.0f, 1.0f));*/
-}
-
-void ToonToggleButton::ToggleState()
-{
-	m_CurrentState = m_CurrentState == ToggleButtonStates::ToggleOff ? ToggleButtonStates::ToggleOn : ToggleButtonStates::ToggleOff;
-}
-
 void ToonToggleButton::DrawValue()
 {
 	Coordinates valueCoordinates = m_Coordinates;
@@ -72,4 +63,9 @@ void ToonToggleButton::DrawValue()
 	valueCoordinates.origin.y	+= (valueCoordinates.size.y / 2.0f) - 1.0f;
 	std::string toggleValue		 = m_CurrentState == ToggleButtonStates::ToggleOff ? "OFF" : "ON";
 	Debug::Print(toggleValue, valueCoordinates.origin, Debug::WHITE);
+}
+
+void ToonToggleButton::ToggleCurrentState()
+{
+	m_CurrentState = m_CurrentState == ToggleButtonStates::ToggleOff ? ToggleButtonStates::ToggleOn : ToggleButtonStates::ToggleOff;
 }
--- CSC8503/ToonToggleButton.h
+++ CSC8503/ToonToggleButton.h
@@ -17,16 +17,80 @@ enum ToggleButtonStates
 	ToggleOn
 };
 
+
+class ToonToggleButtonHead
+{
+	enum AnimationDirection
+	{
+		NoAnimation,
+		LeftToRight,
+		RightToLeft
+	};
+
+	private:
+		Vector2			   m_StartLocation;
+		Vector2			   m_EndLoacation;
+		Vector2			   m_CurrentLocation;
+		Vector2			   m_Size;
+		AnimationDirection m_AnimationDirection;
+		const Vector4	   m_HeadColour		= Debug::WHITE;
+		const float		   m_AnimationSpeed = 0.5f;
+		
+	public:
+		ToonToggleButtonHead(Coordinates coordinates)
+		{
+			m_StartLocation		 = coordinates.origin;
+			m_EndLoacation		 = coordinates.origin + coordinates.size;
+			m_CurrentLocation	 = m_StartLocation;
+			m_Size				 = coordinates.size;
+			m_AnimationDirection = AnimationDirection::NoAnimation;
+		}
+
+		void TriggerAnimation() { UpdateAnimationDirection(); }
+
+		void UpdateHead()
+		{
+			DrawHead();
+		}
+
+	private:
+		void UpdateAnimationDirection()
+		{
+			if (m_CurrentLocation.x == m_StartLocation.x) { m_AnimationDirection = AnimationDirection::LeftToRight; }
+			else { m_AnimationDirection = AnimationDirection::RightToLeft; }
+		}
+
+		float GetIncrement()
+		{
+			if		(m_AnimationDirection == AnimationDirection::LeftToRight) { return  m_AnimationSpeed; }
+			else if (m_AnimationDirection == AnimationDirection::RightToLeft) { return -m_AnimationSpeed; }
+			return 0.0f;
+		}
+
+		void DrawHead()
+		{
+			m_CurrentLocation.x += GetIncrement();
+			Debug::DrawFilledQuad(m_CurrentLocation, m_Size, m_HeadColour);
+			if (m_CurrentLocation.x <= m_StartLocation.x || m_CurrentLocation.x >= m_EndLoacation.x)
+			{
+				m_AnimationDirection = AnimationDirection::NoAnimation;
+			}
+		}
+};
+
+
 class ToonToggleButton
 {
 	private:
-		Coordinates		   m_Coordinates;
-		ToggleButtonStates m_CurrentState;
-		const Vector4	   m_ToggleOnColour  = Debug::GREEN;
-		const Vector4	   m_ToggleOffColour = Vector4(0.5f, 0.5f, 0.5f, 1.0f);
-		const Vector4	   m_HeadColour		 = Debug::WHITE;
-		Coordinates		   m_HalfSize;
-		bool			   m_ShouldDrawValue = false;
+		Coordinates			  m_Coordinates;
+		ToggleButtonStates	  m_CurrentState;
+		const Vector4		  m_ToggleOnColour   = Debug::GREEN;
+		const Vector4		  m_ToggleOffColour  = Vector4(0.5f, 0.5f, 0.5f, 1.0f);
+		const Vector4		  m_HeadColour		 = Debug::WHITE;
+		Coordinates			  m_HalfSize;
+		bool				  m_ShouldDrawValue	 = false;
+		bool				  m_IsAnimating		 = false;
+		ToonToggleButtonHead* m_ToggleButtonHead = NULL;
 
 	public:
 		ToonToggleButton(Coordinates coordinates, bool shouldDrawValue = false);
@@ -39,7 +103,6 @@ class ToonToggleButton
 		void DrawButton();
 		void DrawOffToggle();
 		void DrawOnToggle();
-		void DrawHead();
-		void ToggleState();
 		void DrawValue();
+		void ToggleCurrentState();
 };
