13155fe0bcdcb6220b952c3ff8d753b715732425
Ryan Holt <c2055347@newcastle.ac.uk>
2023-03-03 11:09:13 +0000

    Fix

---
+++ CSC8503/Team.cpp
@@ -0,0 +1,8 @@
+#include "Team.h"
+
+using namespace NCL::CSC8503;
+
+const Vector3 Team::T_GREEN_GOBLINS = Vector3(0.0f, 1.0f, 0.0f);
+const Vector3 Team::T_PURPLE_PRAWNS = Vector3(1.0f, 0.0f, 1.0f);
+const Vector3 Team::T_BLUE_BULLDOGS = Vector3(0.0f, 0.0f, 1.0f);
+const Vector3 Team::T_ORANGE_OTTERS = Vector3(1.0f, 1.0f / 4.0f, 0.0f);
