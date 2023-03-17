d9916bc120771d23ccceab9b7e0d43a3493c0f5c
Sumit Makkar <c2060829@newcastle.ac.uk>
2023-03-17 16:40:03 +0000

    made helper functions.

--- CSC8503/ToonSlider.cpp
+++ CSC8503/ToonSlider.cpp
@@ -1,1 +1,70 @@
 #include "ToonSlider.h"
+
+ToonSlider::ToonSlider(Coordinates coordinates, int sliderLevels, float textWidth, float sliderHeight, float sliderHeadSize) : m_coordinates(coordinates), m_SliderLevels(sliderLevels), m_IsActive(false)
+{
+	AssignCoordinates(textWidth, sliderHeight, sliderHeadSize);
+	PopulateSliderCoordinatesMap();
+}
+
+ToonSlider::~ToonSlider()
+{
+}
+
+void ToonSlider::Update(float dt)
+{
+}
+
+void ToonSlider::DrawSlider()
+{
+
+}
+
+void ToonSlider::DrawBar()
+{
+
+}
+
+void ToonSlider::DrawHead()
+{
+}
+
+void ToonSlider::AssignCoordinates(float textWidth, float sliderHeight, float sliderHeadSize)
+{
+	m_ShouldDisplayLevels	= textWidth > 0.0f;
+	m_SliderTextCoordinates = Coordinates(Vector2(m_coordinates.origin.x + m_coordinates.size.x - textWidth, m_coordinates.origin.y), Vector2(textWidth, m_coordinates.size.y));
+	m_SliderBarCoordinates  = Coordinates(m_coordinates.origin, Vector2(m_coordinates.size.x - textWidth, sliderHeight));
+	//Head
+	m_HeadCoordinates		= GetHeadCoordinates(m_CurrentLevel);
+}
+
+void ToonSlider::PopulateSliderCoordinatesMap()
+{
+	float levelWidth = m_coordinates.size.x / m_SliderLevels;
+	for (int i = 0; i < m_SliderLevels; i++)
+	{
+		float offset = levelWidth * i;
+		m_SliderLevelCoordinatesMap[i] = Coordinates(Vector2(m_coordinates.origin.x + offset, m_coordinates.origin.y), Vector2(levelWidth, m_coordinates.size.y));
+	}
+}
+
+Coordinates ToonSlider::GetHeadCoordinates(int level)
+{
+	if (level < 0 && level >= m_SliderLevels) { return INVALID_COORDINATE; }
+	return m_SliderLevelCoordinatesMap[level];
+}
+
+
+int ToonSlider::GetHeadLevel(Coordinates headCoordinates)
+{
+	int index = 0;
+	for (auto& sliderLevelData : m_SliderLevelCoordinatesMap)
+	{
+		float sliderLevelEndXCoord = sliderLevelData.second.origin.x + sliderLevelData.second.size.x;
+		if (sliderLevelData.second.origin.x >= headCoordinates.origin.x && sliderLevelEndXCoord > headCoordinates.origin.x)
+		{
+			return  index;
+		}
+		index++;
+	}
+	return -1;
+}
--- CSC8503/ToonSlider.h
+++ CSC8503/ToonSlider.h
@@ -1,4 +1,38 @@
 #pragma once
+
+#include <unordered_map>
+#include "GameTechRenderer.h"
+#include "Toon2DCoordinates.h"
+#include "InputManager.h"
+
 class ToonSlider
 {
+	private:
+		Coordinates							 m_coordinates;	//Coordinates of just slider with text space
+		int									 m_SliderLevels;
+		bool								 m_IsActive;
+		bool								 m_ShouldDisplayLevels;
+		Coordinates							 m_SliderBarCoordinates;
+		Coordinates							 m_SliderTextCoordinates;
+		Coordinates							 m_HeadCoordinates;
+		const Vector4&						 m_SliderCoveredColour   = Vector4(0.5f, 0.5f, 0.5f, 1.0f);
+		const Vector4&						 m_SliderUncoveredColour = Debug::GREEN;
+		int									 m_CurrentLevel		     = 0;
+		std::unordered_map<int, Coordinates> m_SliderLevelCoordinatesMap;	// Using it, in order to avoid repetitive calculations.
+		const Coordinates					 INVALID_COORDINATE		 = Coordinates(Vector2(-1.0f, -1.0f), Vector2(-1.0f, -1.0f));
+
+	public:
+		ToonSlider(Coordinates coordinates, int sliderLevels, float textWidth = 10.0f, float sliderHeight = 2.0f, float sliderHeadSize = 5.0f);	//Set text width to 0 if Slider text is not needed
+		~ToonSlider();
+
+		void Update(float dt);
+
+	private:
+		void DrawSlider();
+		void DrawBar();
+		void DrawHead();
+		void AssignCoordinates(float textWidth, float sliderHeight, float sliderHeadSize);
+		void PopulateSliderCoordinatesMap();
+		Coordinates GetHeadCoordinates(int level);
+		int GetHeadLevel(Coordinates headCoordinates);
 };
